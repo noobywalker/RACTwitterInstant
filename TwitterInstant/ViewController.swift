868b07ab96ad8ea2012887cfd2bedfa840046792
Waratnan Suriyasorn <s.waratnan@gmail.com>
2016-06-16 19:08:14 +0700

    Initial Commit

---
+++ TwitterInstant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterInstant
+//
+//  Created by Waratnan Suriyasorn on 6/16/2559 BE.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
